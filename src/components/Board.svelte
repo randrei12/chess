849805b17a48f30c4c337fc729cb284934cf104a
randrei12 <rantaandrei12@gmail.com>
2023-03-16 12:06:42 +0200

    #

--- src/components/Board.svelte
+++ src/components/Board.svelte
@@ -1,27 +1,16 @@
+<script>
+
+    import Cell from "./Cell.svelte";
+
+</script>
 <style lang="scss">
     @use "../scss/board.scss";
 </style>
 
-<!-- <style src="../scss/a.css"></style> -->
 
-<script lang="ts">
-    import { onMount } from "svelte";
-    let root: HTMLDivElement;
-    
-    onMount(() => {
-        for (let i = 0; i < 64; i++) {
-            const color = 1 - i % 2;
-            const [x, y] = [String.fromCharCode(65 + i % 8), Math.floor(i / 8) + 1];
-            let square = document.createElement('div');
-            square.classList.add('cell');
-            square.innerHTML = `${x} ${y} ${color ? 'black' : 'white'}`;
-            root.appendChild(square);
-        }
-    })
-    
-</script>
+<div class="board">
+    { #each Array(64) as _, i }
+        <Cell index={i} />
+    { /each }
 
-
-<div bind:this={root} class="board">
-    
 </div>
--- src/components/Cell.svelte
+++ src/components/Cell.svelte
@@ -1,3 +1,10 @@
+<script lang="ts">
+    export let index: number;
+    const color = 1 - index % 2;
+    const [x, y] = [String.fromCharCode(65 + index % 8), Math.floor(index / 8) + 1];
+    console.log(index);
+</script>
 
-
-<div></div>
+<div>
+    { x } { y } {color ? 'black' : 'white'}
+</div>
